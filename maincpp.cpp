f31b0fc9217c274b4fbb5e19adb42738e8f66e2a
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-06 19:39:13 +0200

    add code

--- maincpp.cpp
+++ maincpp.cpp
@@ -1,1 +1,16 @@
-g
+#include<iostream>
+using namespace std;
+void main()
+{
+   
+    int a,b, max;
+    cout << "Input a:";
+    cin >> a;
+    cout << "Input b:";
+    cin >> b;
+    if (a > b)
+        cout << "number-->" << a << endl;
+    if (a < b)
+        cout << "number-->" << b << endl;
+
+}
