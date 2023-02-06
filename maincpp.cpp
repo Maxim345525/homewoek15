2aec15a6bbf442cc15251617f4974cddec6a98e3
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-06 19:20:43 +0200

    add code

--- maincpp.cpp
+++ maincpp.cpp
@@ -1,1 +1,14 @@
-g
+#include<iostream>
+using namespace std;
+void main()
+{
+	int a;
+	cout << "Input number:";
+	cin >> a;
+	if (a % 1 == 0 || a % a == 0) {
+		cout << "Proste" << endl;
+	}
+	else(a % 1 != 0 || a % a != 0);
+		cout << " Ne proste";
+
+}
