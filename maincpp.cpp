a707ba1bc79a326f0a9dd77fb5fa970a020e411b
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-06 18:25:26 +0200

    add code

--- maincpp.cpp
+++ maincpp.cpp
@@ -1,1 +1,29 @@
-g
+#include<iostream>
+using namespace std;
+void rect(int a, int b);
+
+void main()
+{
+	int length, width;
+	do
+	{
+		cout << "Input width-->";
+		cin >> length;
+		cout << "Input length-->";
+		cin >> width;
+		rect(length, width);
+		cin >> length;
+	} while (length == 1);
+}
+
+void rect(int a, int b)
+{
+	for (int i = 0; i < b; i++)
+	{
+		for (int j = 0; j < a; j++)
+		{
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
